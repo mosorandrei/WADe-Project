8aeda04f4825eb147fb577ba06071d7dcf3433be
DESKTOP-5O8CUVJ\ROG <mosorandrei49@gmail.com>
2024-02-04 17:47:58 +0200

    updated fe host

--- server-side/src/main/java/com/botanical/gardens/serverside/controllers/UserController.java
+++ server-side/src/main/java/com/botanical/gardens/serverside/controllers/UserController.java
@@ -26,7 +26,7 @@ import java.time.LocalTime;
 import java.util.List;
 
 @Getter
-@CrossOrigin(origins = {"http://localhost:5173", "https://65bfad59f775d96f899d7dd1--lustrous-selkie-ce070a.netlify.app"})
+@CrossOrigin(origins = {"http://localhost:5173", "https://botanical-garden-guide.netlify.app"})
 @RequestMapping("/user")
 @RestController
 public class UserController {
